10408198a3cd8278daa39f2ced475ce349cdebb0
Márcio Bortolini <marciobds@live.it>
2021-03-15 11:55:23 -0300

    Comṕatibilidade + traduções + ajustes + última versão solidus

--- app/decorators/core/spree/permission_sets/product_management_decorator.rb
+++ app/decorators/core/spree/permission_sets/product_management_decorator.rb
@@ -1,19 +1,18 @@
 # frozen_string_literal: true
-module SolidusImportProduct
-	module Spree
-		module PermissionSets
-			module ProductManagementDecorator
-			  	# Create an alias_method for the original activate! method as we need to execute it as it is
-			  	# alias_method :super_activate!, :activate!
 
-			  	# Extends the behaviour of the activate! method by calling the original method and adding an extra permission set for Spree::ProductImport
-			  	def activate!
-			    	super
-			    	can :manage, Spree::ProductImport
-			  	end
-			
-				::Spree::PermissionSets::ProductManagement.prepend self
-			end
+module Spree
+	module PermissionSets
+		module ProductManagementDecorator
+		  	# Create an alias_method for the original activate! method as we need to execute it as it is
+		  	# alias_method :super_activate!, :activate!
+
+		  	# Extends the behaviour of the activate! method by calling the original method and adding an extra permission set for Spree::ProductImport
+		  	def activate!
+		    	super
+		    	can :manage, Spree::ProductImport
+		  	end
+		
+			::Spree::PermissionSets::ProductManagement.prepend self
 		end
 	end
 end
